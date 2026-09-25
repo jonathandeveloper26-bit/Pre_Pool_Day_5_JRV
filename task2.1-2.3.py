415337172900440c1847c61f007d2d0b2b4f62a1
jonathandeveloper26-bit <jonathan.developer26@gmail.com>
2026-09-25 10:37:06 +0200

    Task 2.1-2.3: Dictionaries - Creating, Adding to, and re-assingning values

---
+++ task2.1-2.3.py
@@ -0,0 +1,13 @@
+### Task 2.1
+pokemons = {"Pikachu": "Electric", "Bulbasaur": "Grass", "Charmander": "Fire"}
+print(pokemons)
+
+### Task 2.2
+pokemons["Blaziken"] = "Fire"
+print(pokemons)
+
+### Task 2.3
+pokemons["Pikachu"] = ["Pichu", "Raichu"]
+print(pokemons)
+
+### Question: If you assign a value to a key that already exists, it will overwrite the previous value that existed for that key. 

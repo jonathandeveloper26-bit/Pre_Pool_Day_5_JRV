4ec996dab68f23189fb2c14131193fd7f41b51df
jonathandeveloper26-bit <jonathan.developer26@gmail.com>
2026-09-25 10:51:00 +0200

    Task 2.11: Dictionaries - Getting Max Value within Dictionary.

---
+++ task2.11.py
@@ -0,0 +1,22 @@
+### Task 2.11: Get Key with Maximum Value
+
+given_dict = {
+    "dalmations": 101, 
+    "pi": 3.14,
+    "life": 42,
+    "googol": 10**100,
+    "jordan": 23,
+    "life, the universe and everything": 42, 
+    "emergency": 911,
+    "euler": 2.71828
+}
+
+max_key = ""
+max_value = 0
+for key in given_dict:
+    max_key = key if given_dict[key] > max_value else max_key
+    max_value = given_dict[key] if given_dict[key] > max_value else max_value
+
+
+print(f"Max Key: {max_key}")
+print(f"Max Value: {max_value}")
